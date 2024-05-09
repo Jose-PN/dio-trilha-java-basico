1393bbc9a70ff38d66990a08bbbba9bced263c83
José-PN <jos1995nascimento@gmail.com>
2024-05-09 18:07:01 -0300

    corrigindo elegibilidade

--- Exercicios-testes/src/VerificadorElegibilidadeConta.java
+++ Exercicios-testes/src/VerificadorElegibilidadeConta.java
@@ -10,9 +10,9 @@ public class VerificadorElegibilidadeConta {
 
 // TODO: Verifique se a idade é maior ou igual a 18 e imprima uma mensagem informando que o usuário é elegível para criar uma conta bancária:
        if (idade >= 18) {
-        System.out.println("Voce e elegivel para criar uma conta bancaria.");
+        System.out.println("Voce esta elegivel para criar uma conta bancaria.");
        } else {
-        System.out.println("Voce nao e elegivel para criar uma conta bancaria.");
+        System.out.println("Voce nao esta elegivel para criar uma conta bancaria.");
        }
 
 // Fechamos o objeto Scanner para liberar os recursos:
